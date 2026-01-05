411e33fd96843a58182ba790a274d10ec09f8195
carlyngeorge <carlyngeorge06@gmail.com>
2026-01-05 10:24:24 +0400

    Implement matrix transpose functionality

---
+++ TMatrix.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    printf("Enter matrix:\n");
+    for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Transpose:\n");
+    for(int j=0;j<c;j++) {
+        for(int i=0;i<r;i++)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
