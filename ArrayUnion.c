7a03a784d2655ac2daac4f78ca19792f60bd2746
carlyngeorge <carlyngeorge06@gmail.com>
2026-01-05 10:17:34 +0400

    Implement union of two arrays in ArrayUnion.c

---
+++ ArrayUnion.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int arr1[] = {1, 2, 3, 4};
+    int arr2[] = {3, 4, 5, 6};
+    int n1 = 4, n2 = 4;
+    int unionArr[20], k = 0;
+
+    for (int i = 0; i < n1; i++)
+        unionArr[k++] = arr1[i];
+
+    for (int i = 0; i < n2; i++) {
+        int found = 0;
+        for (int j = 0; j < n1; j++) {
+            if (arr2[i] == arr1[j]) {
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+            unionArr[k++] = arr2[i];
+    }
+
+    printf("Union: ");
+    for (int i = 0; i < k; i++)
+        printf("%d ", unionArr[i]);
+
+    printf("\n");
+    return 0;
+}
